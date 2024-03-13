6653cb8d7d4f8b87039dcc5365c6bf0cd52ada72
Jess <jessgatura@gmail.com>
2024-03-13 23:38:17 +0300

    .

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -3,13 +3,14 @@
 
 from flask import Flask, render_template
 
-
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     """renders html page"""
     return render_template('0-index.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
